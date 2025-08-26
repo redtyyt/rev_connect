c519effb290d9249db98d103f4b3d106863fecd8
redtyyt <redty2011@gmail.com>
2025-08-26 14:52:32 +0200

    Add encrypted script and executable for secure execution

---
+++ encrypted.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'encrypted.exe' with Python DLL being found.
+set PATH=c:\python313;%PATH%
+set PYTHONHOME=C:\Python313
+set NUITKA_PYTHONPATH=C:\Users\redty\hacks\reverse_connection;C:\Python313\DLLs;C:\Python313\Lib;C:\Python313;C:\Users\redty\AppData\Roaming\Python\Python313\site-packages;C:\Users\redty\AppData\Roaming\Python\Python313\site-packages\win32;C:\Users\redty\AppData\Roaming\Python\Python313\site-packages\win32\lib;C:\Users\redty\AppData\Roaming\Python\Python313\site-packages\Pythonwin;C:\Python313\Lib\site-packages;C:\Users\redty\AppData\Roaming\Python\Python313\site-packages\setuptools\_vendor
+"%~dp0encrypted.exe" %*
